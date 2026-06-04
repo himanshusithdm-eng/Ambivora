a80d6d2759ac1c1f5680f6cdf47c5f4a416603cd
himanshusithdm-eng <himanshu.sith.dm@gmail.com>
2026-06-04 15:48:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+const menu=document.querySelector('.menu');const links=document.querySelector('.links');if(menu){menu.addEventListener('click',()=>links.classList.toggle('open'))}
+const items=document.querySelectorAll('.reveal');const obs=new IntersectionObserver((entries)=>{entries.forEach(e=>{if(e.isIntersecting){e.target.classList.add('show');obs.unobserve(e.target)}})},{threshold:.12});items.forEach(i=>obs.observe(i));
